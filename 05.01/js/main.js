0d975e42152cb3f94d44ad9f5657f7b9e05ea090
NISO <nissanjobim@gmail.com>
2023-05-12 00:49:19 +0300

    add additional option

--- 05.01/js/main.js
+++ 05.01/js/main.js
@@ -1,18 +1,30 @@
-const Student = [];
-let repeat = true;
+// //option 1: I don't limit the user enter student names
+// const Student = [];
+// let repeat = true;
 
-while (repeat) {
+// while (repeat) {
+//   let studentName = prompt("Enter a student name please: ");
+//   if (studentName) {
+//     Student.push(studentName.toLowerCase().replace(" ",""));
+//     repeat = confirm("Do you want add another student?");
+//   } else {
+//     repeat = false;
+//   }
+// }
+
+// if (Student.length > 0) {
+//   alert("Here are all students after sorting: " + Student.sort().join(", "));
+// } else {
+//   alert("There is no list to show!");
+// }
+
+// //option 2: the user can enter 5 names only
+const Students = [];
+
+for (let i = 0; i < 5; i++) {
   let studentName = prompt("Enter a student name please: ");
-  if (studentName) {
-    Student.push(studentName.toLowerCase().replace(" ",""));
-    repeat = confirm("Do you want add another student?");
-  } else {
-    repeat = false;
-  }
+  Students.push(studentName.toLowerCase().replace(" ",""));
 }
+alert("Here are all students after sorting: " + Students.sort().join(", "));
 
-if (Student.length > 0) {
-  alert("Here are all students after sorting: " + Student.sort().join(", "));
-} else {
-  alert("There is no list to show!");
-}
+
